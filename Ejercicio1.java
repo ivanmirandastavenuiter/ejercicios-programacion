a9c248abb29096e63e4a729fb542d4c6a554f5a0
Iván Miranda Stavenuiter <ivanmist90@gmail.com>
2017-10-05 10:27:28 +0200

    Añadida la actualización del ejercicio 1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,5 +6,6 @@
 public class Ejercicio1 { // Clase principal
 	public static void main(String[] args) {
 		System.out.println("Iván Miranda Stavenuiter");
+    System.out.println("Programa realizado por Iván Miranda Stavenuiter");
 	}
 }
