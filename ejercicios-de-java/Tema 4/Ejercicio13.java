9dd73e47eb6ef388bb0f49acad1a5d9751f78fac
Iván Miranda Stavenuiter <ivanmist90@gmail.com>
2017-10-19 18:00:17 +0200

    Ejercicio 13

--- ejercicios-de-java/Tema 4/Ejercicio13.java
+++ ejercicios-de-java/Tema 4/Ejercicio13.java
@@ -12,8 +12,23 @@ public class Ejercicio13 {
     int dos = Integer.parseInt(System.console().readLine());
     int tres = Integer.parseInt(System.console().readLine());
     
-    if ((uno < dos) && (uno < tres)) {
+    if ((uno < dos) && (dos < tres)) {
       System.out.println(uno + ", " + dos + ", " + tres);
+    }
+    if ((uno < tres) && (tres < dos)) {
+      System.out.println(uno + ", " + tres + ", " + dos);
+    }
+    if ((dos < uno) && (uno < tres)) {
+      System.out.println(dos + ", " + uno + ", " + tres);
+    }
+    if ((dos < tres) && (tres < uno)) {
+      System.out.println(dos + ", " + tres + ", " + uno);
+    }
+    if ((tres < dos) && (dos < uno)) {
+      System.out.println(tres + ", " + dos + ", " + uno);
+    }
+    if ((tres < uno) && (uno < dos)) {
+      System.out.println(tres + ", " + uno + ", " + dos);
     }
   }
 }
