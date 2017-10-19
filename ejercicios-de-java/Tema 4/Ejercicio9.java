09831f7423e1ad45cb4ed0d56d6fa30eb4ec58cd
Iván Miranda Stavenuiter <ivanmist90@gmail.com>
2017-10-19 09:37:18 +0200

    Comentarios aclaratorios ejercicio 9

--- ejercicios-de-java/Tema 4/Ejercicio9.java
+++ ejercicios-de-java/Tema 4/Ejercicio9.java
@@ -2,6 +2,20 @@
 * Ejercicio9
 * Si usamos System.console().readLine(); más .toLowercase(), se pasa
 * todo a minúscula.
+* 
+* Lo primero es calcular discriminante (número que va dentro de la raíz).
+* En caso de ser negativo, esta ni se realiza, puesto que no tiene
+* solución.
+* 
+* En la solución, Luis propone como alternativas (else if) valores de las
+* incógnitas iguales a 0 o distintas a 0. En el caso de que a, b y c fuesen
+* 0, la solución es una identidad (0=0).
+* Si son distintos a 0, dado que ya está el if discriminante < 0, estos
+* van a ser distintos pero positivos, con lo que se podrá calcular. 
+* 
+* Entonces, lo que tienes que hacer es cuándo cada incógnita vale cada cosa.
+* Es decir, iguales a cero y distintos a cero.
+* 
 * @author Iván Miranda Stavenuiter
 */
 
