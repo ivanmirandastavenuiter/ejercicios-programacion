d5ba778b8f5dc88e471d9f3164599ce2b4299eef
Iván Miranda Stavenuiter <ivanmist90@gmail.com>
2017-10-27 09:42:51 +0200

    Bucles

---
+++ ejercicios-de-java/Tema 5/Ejercicio4.java
@@ -0,0 +1,17 @@
+/**
+* Ejercicio4
+* 
+* @author Iván Miranda Stavenuiter
+*/
+
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    
+    for (int i = 320; i >= 160; i = i -20) {
+      
+      if (i % 5 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
---
+++ ejercicios-de-java/Tema 5/Ejercicio5.java
@@ -0,0 +1,18 @@
+/**
+* Ejercicio5
+* 
+* @author Iván Miranda Stavenuiter
+*/
+
+public class Ejercicio5 {
+  public static void main(String[] args) {
+    
+    int i = 320;
+    
+    while (i >= 160) {
+      System.out.println(i);
+      i = i - 20;
+    }
+  }
+}
+    
